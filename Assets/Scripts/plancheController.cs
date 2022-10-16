7f48b7d19b5ea148ad5c2bb8de7e24a37614b3dc
PierreVandel <pierre.vdl@orange.fr>
2022-10-16 17:00:07 +0200

    Created planche bascule controller script

---
+++ Assets/Scripts/plancheController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class plancheController : MonoBehaviour
+{
+    //Privates variables reachable in the inspector
+    [SerializeField] private Vector2 rotationXZ;    //Vector modified by the controller
+    [SerializeField] private Transform plancheMesh;
+    [SerializeField] private float speed = 0.01f;
+
+    //Privates variables not reachable in the inspector
+    private float timeCount = 0.0f;
+    private GameObject targetRotation;  //Used in Lerp function as target rotation
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initialization
+        rotationXZ = new Vector2(0.0f, 0.0f);
+        targetRotation = new GameObject("targetRotationGO");
+        targetRotation.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetRotation.transform.rotation = Quaternion.Euler(rotationXZ.x, targetRotation.transform.rotation.y, rotationXZ.y);
+
+        //Interpolates between plancheMesh.rotation and targetRotation.transform.rotation by timeCount * speed and normalizes the result afterwards
+        plancheMesh.rotation = Quaternion.Lerp(plancheMesh.rotation, targetRotation.transform.rotation, timeCount * speed);
+
+        timeCount = timeCount + Time.deltaTime;
+    }
+}
