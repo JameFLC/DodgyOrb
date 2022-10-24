1e4281e87c0ab274d4ceadcd9153069b71011af1
JameFLC <jame.flochlecarour@gmail.com>
2022-10-24 19:56:31 +0200

    Ported Plank rotation to rigidbody phyisics and updated gravity

--- Assets/Scripts/PivotXYController.cs
+++ Assets/Scripts/PivotXYController.cs
@@ -10,7 +10,7 @@ public class PivotXYController : MonoBehaviour
 
     //Privates variables not reachable in the inspector
     private GameObject targetRotation;  //Used in Lerp function as target rotation
-
+    private Rigidbody _rigidbody;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +18,7 @@ public class PivotXYController : MonoBehaviour
         rotationAxis = new Vector2(0.0f, 0.0f);
         targetRotation = new GameObject("targetRotationGO");
         targetRotation.transform.parent = transform;
+        _rigidbody = pivot.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -26,7 +27,7 @@ public class PivotXYController : MonoBehaviour
         targetRotation.transform.localRotation = Quaternion.Euler(rotationAxis.y * maxAngle, 0, -rotationAxis.x * maxAngle);
 
         //Interpolates between pivot.rotation and targetRotation.transform.rotation by timeCount * speed and normalizes the result afterwards
-        pivot.localRotation = Quaternion.Lerp(pivot.localRotation, targetRotation.transform.localRotation, Time.deltaTime * speed);
+        _rigidbody.MoveRotation(Quaternion.Lerp(pivot.localRotation, targetRotation.transform.localRotation, Time.deltaTime * speed));
     }
     public void GetData(Vector2 data)
     {
