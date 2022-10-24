cdd1671e04c87aad4e3864836eda338be5c1eae3
JameFLC <jame.flochlecarour@gmail.com>
2022-10-24 19:43:54 +0200

    Fixed Weird file name issue plus obstacle control testing
    Plank issues fixed

---
+++ Assets/Scripts/PivotXYController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PivotXYController : MonoBehaviour
+{
+    //Privates variables reachable in the inspector
+    [SerializeField] private Vector2 rotationAxis;    //Vector modified by the controller
+    [SerializeField] private float maxAngle = 15f;
+    [SerializeField] private Transform pivot;
+    [SerializeField] private float speed = 1f;
+
+    //Privates variables not reachable in the inspector
+    private GameObject targetRotation;  //Used in Lerp function as target rotation
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initialization
+        rotationAxis = new Vector2(0.0f, 0.0f);
+        targetRotation = new GameObject("targetRotationGO");
+        targetRotation.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetRotation.transform.localRotation = Quaternion.Euler(rotationAxis.y * maxAngle, 0, -rotationAxis.x * maxAngle);
+
+        //Interpolates between pivot.rotation and targetRotation.transform.rotation by timeCount * speed and normalizes the result afterwards
+        pivot.localRotation = Quaternion.Lerp(pivot.localRotation, targetRotation.transform.localRotation, Time.deltaTime * speed);
+    }
+    public void GetData(Vector2 data)
+    {
+        rotationAxis = data;
+        Debug.Log(data);
+    }
+}
