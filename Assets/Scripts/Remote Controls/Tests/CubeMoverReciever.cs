9bc44d327424512bf35e80e02fc2a261bb505ae4
Jame Floc'h Le Carour <jame.flochlecarour@gmail.com>
2022-11-19 23:01:35 +0100

    Fixed Rotatable Button

--- Assets/Scripts/Remote Controls/Tests/CubeMoverReciever.cs
+++ Assets/Scripts/Remote Controls/Tests/CubeMoverReciever.cs
@@ -12,7 +12,7 @@ namespace DodgyOrb.RemoteControls
 
         public void MoveCube(Vector2 data)
         {
-            Debug.Log("Data Recived : " + data);
+            //Debug.Log("Data Recived : " + data);
             // Convert normalized data to the cube Range coordinates
             float newX = lockXAxis ? 0 :cubeRange.x + data.x * (cubeRange.y - cubeRange.x);
             float newY = lockYAxis ? 0 : cubeRange.z + data.y * (cubeRange.w - cubeRange.z);
