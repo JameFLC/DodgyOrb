9fabc82f9e57c4c053a00e93697e115cb40d6fd8
JameFLC <jame.flochlecarour@gmail.com>
2022-11-23 14:16:40 +0100

    Implemented bell sound and aded first orb sound

---
+++ Assets/Scripts/Sound/AudioSourceTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace DodgyOrb
+{
+    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioSourceTrigger : MonoBehaviour
+    {
+        // Parameters
+        [SerializeField] private string tagToCheck = "Orb";
+        // Variables
+        private AudioSource _source;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _source = GetComponent<AudioSource>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == tagToCheck)
+            {
+                _source.PlayOneShot(_source.clip);
+                Debug.Log(_source.clip.name + " clip was played");
+            }
+        }
+    }
+}
