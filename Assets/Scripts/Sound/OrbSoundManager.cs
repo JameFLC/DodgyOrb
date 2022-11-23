7ee202af3b7d0376cc51674654773d2b4a3b141e
Jame Floc'h Le Carour <jame.flochlecarour@gmail.com>
2022-11-23 20:48:34 +0100

    Added collision sounds and fbx exporter package

--- Assets/Scripts/Sound/OrbSoundManager.cs
+++ Assets/Scripts/Sound/OrbSoundManager.cs
@@ -9,24 +9,28 @@ namespace DodgyOrb
     public class OrbSoundManager : MonoBehaviour
     {
         [SerializeField] private Vector2 velocityRange = new Vector2(0, 5);
+        [SerializeField] private Vector2 collisionVelocityRange = new Vector2(3, 9);
         [Space]
-        [SerializeField] private Vector2 pitchRange = new Vector2(1,2);
+        [SerializeField] private Vector2 pitchRange = new Vector2(1, 2);
         [SerializeField] private AnimationCurve pitchResponse = new();
         [Space]
         [SerializeField] private Vector2 volumeRange = new Vector2(0, 1);
         [SerializeField] private AnimationCurve volumeResponse = new();
         [SerializeField] private float inAirFadeSpeed = 40;
+        [SerializeField] private AudioSource loopSource;
+        [Space]
+        [SerializeField] private AudioSource collisionSource;
+        [SerializeField] private Vector2 collisionPitchRange = new Vector2(1,1.5f);
+        [SerializeField] private AnimationCurve collisionResponse = new();
         // Variables
         private Rigidbody _RB;
-        private AudioSource _source;
         private List<GameObject> _colliders = new();
         private float _collisionBasedVolume = 0;
         // Start is called before the first frame update
         void Start()
         {
             _RB = GetComponent<Rigidbody>();
-            _source = GetComponent<AudioSource>();
-            _source.volume = 0;
+            loopSource.volume = 0;
         }
 
         // Update is called once per frame
@@ -36,14 +40,14 @@ namespace DodgyOrb
 
             float normalizedMagnitude = Normalize(velocity.magnitude, velocityRange[0], velocityRange[1]);
 
-            _source.pitch = Mathf.Lerp(pitchRange[0], pitchRange[1], pitchResponse.Evaluate(normalizedMagnitude));
+            loopSource.pitch = Mathf.Lerp(pitchRange[0], pitchRange[1], pitchResponse.Evaluate(normalizedMagnitude));
             float speedBasedVolume = Mathf.Lerp(volumeRange[0], volumeRange[1], volumeResponse.Evaluate(normalizedMagnitude));
 
             _collisionBasedVolume = IsOrbInMidAir() ? Mathf.Lerp(_collisionBasedVolume, 0, Time.deltaTime * inAirFadeSpeed): 1;
 
-            _source.volume = speedBasedVolume * _collisionBasedVolume;
+            loopSource.volume = speedBasedVolume * _collisionBasedVolume;
+            
 
-            Debug.LogFormat(IsOrbInMidAir() ? "True": "false");
         }
 
         private float Normalize(float value, float from1, float to1)
@@ -56,8 +60,21 @@ namespace DodgyOrb
             {
                 _colliders.Add(collision.gameObject);
             }
-
+            if (collision.relativeVelocity.magnitude > collisionVelocityRange[0])
+            {
+                PlayCollisionSound(collision);
+            }
         }
+
+        private void PlayCollisionSound(Collision collision)
+        {
+            float normalizedCollisionMagnitude = Normalize(Mathf.Clamp(collision.relativeVelocity.magnitude,0, collisionVelocityRange[1]),0, collisionVelocityRange[1]);
+            collisionSource.pitch = Random.Range(collisionPitchRange[0], collisionPitchRange[1]) + normalizedCollisionMagnitude / 0.3f;
+
+            collisionSource.volume = collisionResponse.Evaluate(normalizedCollisionMagnitude);
+            collisionSource.PlayOneShot(collisionSource.clip);
+        }
+
         private void OnCollisionExit(Collision collision)
         {
             if (_colliders.Contains(collision.gameObject))
